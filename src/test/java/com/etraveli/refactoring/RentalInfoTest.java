86dfd6f28efa994922e5a048b16613c9d9535a85
Hamid Farmani <hamidfarmani1@gmail.com>
2022-05-18 19:33:00 +0200

    feat: setup project scaffolding
    The structure of project changed in a way that files are now in proper packages.
    The tests are now in their own path.
    Maven dependencies for test has been added.

---
+++ src/test/java/com/etraveli/refactoring/RentalInfoTest.java
@@ -0,0 +1,34 @@
+package com.etraveli.refactoring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.etraveli.refactoring.model.Customer;
+import com.etraveli.refactoring.model.MovieRental;
+import com.etraveli.refactoring.service.RentalInfo;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class RentalInfoTest {
+  @Mock protected RentalInfo rentalInfo;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  void testGet() {
+    String expected =
+        "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+    List<MovieRental> movieRentals =
+        List.of(new MovieRental("F001", 3), new MovieRental("F002", 1));
+    Customer customer = new Customer("C. U. Stomer", movieRentals);
+
+    String result = new RentalInfo().statement(customer);
+
+    assertThat(result).isEqualTo(expected);
+  }
+}
