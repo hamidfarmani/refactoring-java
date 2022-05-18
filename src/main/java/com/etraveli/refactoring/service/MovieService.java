858d4e29d69e9a265dd08685cc90b96ff75117b8
Hamid Farmani <hamidfarmani1@gmail.com>
2022-05-18 21:47:59 +0200

    refactor: rentalinfo business logic
    Business logic for rental movies got split up by different methods. Using BigDecimal for money-related logics.
    Constant values got separated.
    Log4J added.

---
+++ src/main/java/com/etraveli/refactoring/service/MovieService.java
@@ -0,0 +1,39 @@
+package com.etraveli.refactoring.service;
+
+import static com.etraveli.refactoring.utils.enums.MovieCode.*;
+
+import com.etraveli.refactoring.model.Movie;
+import com.etraveli.refactoring.utils.enums.MovieCode;
+import java.util.HashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MovieService {
+  private final Logger LOGGER = LogManager.getLogger(MovieService.class);
+  private HashMap<String, Movie> movies = new HashMap();
+
+  public MovieService() {
+    setMovies();
+  }
+
+  public Movie saveMovie(String title, MovieCode code) {
+    LOGGER.info("Saving movie '{}' with the code of '{}'", title, code);
+    Movie movie = new Movie(title, code);
+    return movie;
+  }
+
+  public void setMovies() {
+    movies.put("F001", saveMovie("You've Got Mail", REGULAR));
+    movies.put("F002", saveMovie("Matrix", REGULAR));
+    movies.put("F003", saveMovie("Cars", CHILDREN));
+    movies.put("F004", saveMovie("Fast & Furious X", NEW));
+  }
+
+  public HashMap<String, Movie> getMovies() {
+    return movies;
+  }
+
+  public Movie getMovie(String movieRentalId) {
+    return movies.get(movieRentalId);
+  }
+}
